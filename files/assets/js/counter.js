e8dbc10f2036b96156666fe55b38cb358ca16566
PN <pn@klapproth-koch.de>
2024-04-19 08:10:55 +0200

    mobile Stylesheet

--- files/assets/js/counter.js
+++ files/assets/js/counter.js
@@ -5,12 +5,11 @@ const memberAll = document.querySelectorAll('.ce_rsce_my_tree');
 memberAll.forEach(element => {
     let numberTree = element.dataset.number;
     const intNumber = parseInt(numberTree) || 0;
-    console.log(intNumber);
     sum += intNumber;
-    console.log(sum + " " + "summe");
 })
 
 counter.innerHTML = sum;
+
 
 
 
