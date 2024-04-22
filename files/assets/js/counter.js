a1a473a5878f70eb997a2a403b78b8bd6c028c69
PN <pn@klapproth-koch.de>
2024-04-22 08:07:05 +0200

    mobile Stylesheet

--- files/assets/js/counter.js
+++ files/assets/js/counter.js
@@ -1,14 +1,28 @@
 const counter = document.getElementById("counter");
 
 let sum = 0;
+let sumString;
 const memberAll = document.querySelectorAll('.ce_rsce_my_tree');
 memberAll.forEach(element => {
     let numberTree = element.dataset.number;
     const intNumber = parseInt(numberTree) || 0;
     sum += intNumber;
+    sumString = sum.toString();
 })
 
-counter.innerHTML = sum;
+function SumString () {
+    const zeroSpan = document.createElement('span');
+    zeroSpan.innerHTML = '0';
+    counter.appendChild(zeroSpan)
+    for(let i = 0; i < sumString.length; i++) {
+        console.log(sumString[i])
+        const span = document.createElement('span');
+        span.innerHTML = sumString[i];
+        counter.appendChild(span);
+    }
+}
+
+SumString();
 
 
 
--- files/assets/js/tree.js
+++ files/assets/js/tree.js
@@ -1,21 +1,28 @@
 const allTree = document.querySelectorAll('.tree-top');
 const allTreePopups = document.querySelectorAll('.tree-bottom');
+const dottedLine = document.querySelectorAll('.dotted-line');
 const popUpClose = document.querySelectorAll('.popup-close');
 const dots = document.querySelectorAll('.dot');
 const overlay = document.querySelector('.tree-overlay');
+const block = document.querySelectorAll('.ce_rsce_my_tree');
+console.log(allTree);
+console.log(block)
 
 allTree.forEach(tree => {
     tree.addEventListener('click', event => {
         let treePopUp = tree.nextElementSibling;
         let treeDot = tree.firstElementChild;
-        console.log(treeDot);
+        let line = tree.offsetParent.lastElementChild;
+        console.log(line)
         if (treePopUp.style.display === "block") {
             treePopUp.style.display = "none";
             overlay.style.display = 'none';
+            line.style.display = "none";
         } else {
             treePopUp.style.display = "block";
             overlay.style.display = 'block';
-            treeDot.classList.add('active')
+            treeDot.classList.add('active');
+            line.style.display = "block";
 
         }
     });
@@ -31,6 +38,9 @@ popUpClose.forEach(close => {
             dot.classList.remove('active')
         })
         overlay.style.display = 'none';
+        dottedLine.forEach(line => {
+            line.style.display = "none";
+        })
 
     })
 })
